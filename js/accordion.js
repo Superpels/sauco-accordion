8e5132cd2d56b59f9e76056ae4efbf13ca8b8cec
Pels Sauco <antonio@sauco-web.com>
2017-02-21 14:03:05 +0100

    Update accordion.js

--- js/accordion.js
+++ js/accordion.js
@@ -4,9 +4,8 @@
 
 jQuery(document).ready(function($) {
 	$( ".sauco-acordeon" ).accordion({
-		collapsible: true
-		/*
 		collapsible: true,
-		active: false // Con este último parámetro hacemos que todos los apartados del acodeón se muestren cerrados*/
+		active: false,
+		heightStyle: "content"
 	});
 });
